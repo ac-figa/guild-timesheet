3b9782f4ff6874ec3ef06fa9237d6a3431ba7cd4
ac-figa <matteobruzzese@hotmail.com>
2026-09-11 23:55:29 -0400

    Update APPS_SCRIPT_URL in config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Paste the URL you get from Apps Script "Deploy > New deployment > Web app" here.
 // It looks like: https://script.google.com/macros/s/AKfycb.../exec
-window.APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxpeqoOPuuSPZ3_p-oU41gnEndpSEQ7OjxZI-9jKQqQ2KISnUcFxv-6vFBQIrvxsgO6/exec';
+window.APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxpeqoOPuuSPZ3_p-oU41gnEndpSEQ7OjxZI-9jKQqQ2KISuNcFxv-6vFBQIrvxsgO6/exec';
